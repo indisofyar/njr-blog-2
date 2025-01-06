d59337106d9257328b1f4312b2f3959d094bada7
indisofyar <38954340+indisofyar@users.noreply.github.com>
2025-01-06 06:27:38 +0000

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from blog.models import BlogPage
+
+
 # Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(BlogPage, BlogAdmin)
